7e60fe5043ad6c29fbf95191e17e2bcb78df7625
Ahmedhesham78 <ahmadhesham9876@gmail.com>
2024-12-05 12:18:16 +0100

    3rd commit

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ const Home = () => {
       {/* text */}
       <div className=" w-full h-[95%] bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto lg:flex-row lg:justify-between lg:pt-20">
-          <div className="xs:mt-[2rem]">
+          <div className="mt-[2.5rem] ">
             <motion.h1
               variants={fadeIn("down", 0.2)}
               initial="hidden"
@@ -31,13 +31,13 @@ const Home = () => {
             </motion.h1>
 
             {/* subtitle */}
-            <motion.p variants={fadeIn("down", 0.3)} initial="hidden" animate="show" exit="hidden" className=" max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-8 xl:mb-16  sm:text-[14px]  md:text-[18px] lg:text-[20px] lg:mx-0 xs:text-[12px] xs:mb-0  ">
+            <motion.p variants={fadeIn("down", 0.3)} initial="hidden" animate="show" exit="hidden" className=" max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-8 xl:mb-16  sm:text-[14px]  md:text-[18px] lg:text-[20px] lg:mx-0 xs:text-[12px] xs:mb-0 pt-2  ">
               We provide full-stack web development solutions using the MERN Stack, combining high performance and innovative design to transform your ideas into modern, standout web applications.
             </motion.p>
           </div>
 
           {/* Btn */}
-          <div className="flex justify-center xl:hidden relative md:x-[100%] lg:items-center z-50000 ">
+          <div className="flex justify-center xl:hidden relative md:x-[100%] lg:items-center z-50 ">
             <ProjectsBtn />
           </div>
           <motion.div variants={fadeIn("down", 0.4)} initial="hidden" animate="show" exit="hidden" className="hidden ">
